d6446063571ba09b0f18ad9cddc1941d210f48a9
Piyush-ops962 <81963977+Piyush-ops962@users.noreply.github.com>
2022-06-30 23:16:29 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,27 +1,13 @@
-"""Hello URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from telnetlib import LOGOUT
 from django.contrib import admin
 from django.urls import path,include
-
-admin.site.site_header="Harry Ice Cream admin"
-admin.site.site_title="Harry Ice Cream admin portal"
-admin.site.index_title="Welcome to Harry Ice Cream"
+from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('home.urls'))
-    
+    path("",views.index,name='home'),
+    path('login',views.login,name="login"),
+    path('logout',views.logout,name='logout'),
+    path("services",views.services,name='services'),
+    path("contact",views.contact,name='contact'),
+    path("about",views.about,name='about')
 ]
